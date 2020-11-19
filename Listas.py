1f4f97d23e26cc42c36ecb75a81974be158c2f9a
Roberto Melesio <60664677+RobertoMel36@users.noreply.github.com>
2020-11-19 12:55:16 -0600

    Add files via upload

--- Listas.py
+++ Listas.py
@@ -19,15 +19,43 @@ class  LinkedList:
             while curr_node.siguiente != None:
                 curr_node = curr_node.siguiente
             curr_node.siguiente = nuevo
+    
     def transversal(self):
         curr_node = self.__head
         while curr_node != None:
-            print(f" {curr_node.data } ->", end="|")
+            print(f" {curr_node.data } ->", end="")
             curr_node = curr_node.siguiente
         print("")
 
+    def tail(self):
+        curr_node = self.__head
+        while curr_node.siguiente != None:
+            curr_node = curr_node.siguiente
+        return curr_node
+
     def remove (self, value):
         curr_node = self.__head
-        while curr_node.data != value and curr_node.siguiente != None:
-            curr_node= curr_node.siguiente
-        if curr_node.data == value:
+        if self.__head.data == value:
+            self.__head = self.__head.siguiente
+        else:
+            anterior = None
+            while curr_node.data != value and curr_node.siguiente != None:
+                anterior = curr_node
+                curr_node= curr_node.siguiente
+            if curr_node.data == value:
+                anterior.siguiente = curr_node.siguiente
+            else: 
+                print("El dato no existe en la lista")
+    
+    def preppend(self, value):
+        nuevo=Nodo( value , self.__head)
+        self.__head=nuevo
+
+    def get( self, posicion=None ): #Regresa el ultimo valor por defecto
+        contador = 0
+        dato= None
+        if posicion ==None:
+            dato = self.tail().data
+        else:
+            pass
+        return dato
